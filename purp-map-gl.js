93386f881d1eb5439b205472ca9234f12efdffa9
Marcin Zukowski <marcin.zukowski@gmail.com>
2021-08-23 23:27:49 -0700

    GL problems solved, I think! :)

--- purp-map-gl.js
+++ purp-map-gl.js
@@ -5,8 +5,7 @@ function runGL(withMaps)
     var renderer, camera, uniforms;
     var bufferTexture
 
-    var grad, grad2;
-    var shaderMat;
+    var grad;
 
     var webglOverlayView;
 
@@ -16,31 +15,42 @@ function runGL(withMaps)
 varying vec2 vUv;
 
 void main() {
-//    gl_Position = vec4(position, 1.0);
+    gl_Position = vec4(position, 1.0);
     vUv = uv;
-
-    gl_Position =   projectionMatrix * 
-                    modelViewMatrix * 
-                    vec4(position,1.0);
 }
 `;
 
     var fragmentShader = `
 #define GLSLIFY 1
-uniform sampler2D u_texture;
 
+uniform sampler2D u_texture;
 varying vec2 vUv;
 
-void main() {
-    vec2 v = gl_FragCoord.xy;
-    float MUL = 0.0;
-    vec3 pixel_color = vec3(fract((v.x + 3.0 * v.y) / 30.0) * MUL, fract(v.x / 50.0) * MUL, fract(v.y / 90.0) * MUL);
+// All components are in the range [0…1], including hue.
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
-    pixel_color += texture2D(u_texture, vUv).rgb;
-    float alpha = texture2D(u_texture, vUv).a;
+void main() {
+    // Compute pixel color. 
+    vec3 pixel_color = texture2D(u_texture, vUv).rgb;
     
-    alpha = alpha < 0.001 ? 0.0 : 1.0;
-    pixel_color *= 10.0; 
+    // Adjust to range GREEN..PURPLE - note, we need to wrap around HSV 
+    const float GREEN = 1.0 + 1.0/3.0;
+    const float PURPLE = 4.0/6.0;
+    const float RANGE = GREEN - PURPLE;
+    float H = pixel_color.r / pixel_color.g;  // in range 0..1
+    H = fract(GREEN - H * RANGE);
+
+    vec3 hsv = vec3(H, 1.0, 1.0);
+    pixel_color = hsv2rgb(hsv);
+    
+    float CUTOFF = 0.1;
+    float alpha = texture2D(u_texture, vUv).a;
+    alpha = alpha < CUTOFF ? alpha / CUTOFF : 1.0;
     
     gl_FragColor = vec4(pixel_color, alpha);
 }    
@@ -49,16 +59,12 @@ void main() {
     async function loadGrad()
     {
         await new Promise(resolve => {
-            grad = new THREE.TextureLoader().load('radial-gradient.png', resolve);
-        });
-        await new Promise(resolve => {
-            grad2 = new THREE.TextureLoader().load('radial-gradient2.png', resolve);
+            grad = new THREE.TextureLoader().load('radial-gradient-pow2.png', resolve);
         });
     }
 
-    async function initScene() {
-
-        console.log("calling loadGraad")
+    async function initScene()
+    {
         await loadGrad();
         console.log("called loadGrad")
 
@@ -66,24 +72,22 @@ void main() {
         camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
 
         // Create the texture that will store our result
-        bufferTexture = new THREE.WebGLRenderTarget( 512, 512, {
+        bufferTexture = new THREE.WebGLRenderTarget(512, 512, {
             minFilter: THREE.LinearFilter,
             magFilter: THREE.NearestFilter,
-            wrapS: THREE.RepeatWrapping});
-
-        // Create the shader material
-        shaderMat = new THREE.ShaderMaterial({
-            uniforms: uniforms,
-            vertexShader : vertexShader,
-            fragmentShader: fragmentShader,
-            transparent: true
+            type: THREE.FloatType,
         });
     }
 
-    var doneCounter = 100;
     var frame = 0;
 
     function render() {
+        frame += 1
+        if (frame > 100) {
+            return;
+        }
+        const add = frame * 0.03;
+
         renderer.setRenderTarget(bufferTexture);
         renderer.clear();
 
@@ -93,42 +97,30 @@ void main() {
         const SIZE = 0.8;
         const square = new THREE.PlaneBufferGeometry(SIZE, SIZE);
         const mat = new THREE.MeshBasicMaterial({
-            alphaMap: grad2,
-            alphaTest: 0.001,
+            alphaMap: grad,
+//            alphaTest: 0.05,
             blending: THREE.CustomBlending,
             blendEquation: THREE.AddEquation,
             blendSrc: THREE.SrcAlphaFactor,
-            blendDst: THREE.DstAlphaFactor,
+            blendDst: THREE.OneFactor,
             blendEquationAlpha: THREE.AddEquation,
             blendSrcAlpha: THREE.OneFactor,
             blendDstAlpha: THREE.OneFactor,
-            opacity: 0.2,
+            opacity: 0.5,
             transparent: true,
         });
 
         let mesh = new THREE.Mesh(square, mat);
         tmpScene.add(mesh);
 
-        frame += 1
-
-        if (frame > 100) {
-            return;
-        }
-
-        const add = frame * 0.07;
-
-        let COUNT = 3;
-        let LUM = 0.6;
+        let COUNT = 10;
+        let FRAC = 0.05;
         for (let i = 0; i < COUNT; i++) {
             mesh.position.setX((Math.sin(add + i * 1.0) + Math.sin(add + i * 1.1)) / 2.5);
             mesh.position.setY((Math.sin(add + i * 1.4) + Math.sin(add + i * 1.3)) / 2.5);
-            mat.color.setHSL(0.25 + i * 0.5/COUNT, 1.0, LUM);
+            mat.color.setRGB(FRAC * i / (COUNT - 1 || 1), FRAC, 1.0);
             renderer.render(tmpScene, camera);
         }
-
-//        console.log(shaderMat);
-//        shaderMat.uniforms.u_texture.value = bufferTexture.texture;
-//        shaderMat.uniforms.u_texture.texture = bufferTexture.texture;
 
         // Define the shader uniforms
         uniforms = {
@@ -138,10 +130,7 @@ void main() {
             }
         };
 
-//        uniforms.u_texture.value = grad;
-//        uniforms.u_texture.texture = grad;
-
-        shaderMat = new THREE.ShaderMaterial({
+        const shaderMat = new THREE.ShaderMaterial({
             uniforms: uniforms,
             vertexShader : vertexShader,
             fragmentShader: fragmentShader,
@@ -152,13 +141,6 @@ void main() {
         const scene2 = new THREE.Scene();
 
         const geo2 = new THREE.PlaneBufferGeometry(0.5, 1.2);
-        const mat2 = new THREE.MeshBasicMaterial({
-            map: bufferTexture.texture,
-            blending: THREE.CustomBlending,
-            blendEquation: THREE.AddEquation,
-            blendSrc: THREE.OneFactor,
-            blendDst: THREE.ZeroFactor,
-        });
         const mesh2 = new THREE.Mesh(geo2, shaderMat);
         scene2.add(mesh2);
 
