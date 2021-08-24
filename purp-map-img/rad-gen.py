8d4d43a24ff3e5e77f259ebb8c38f51a53ec4d9c
Marcin Zukowski <marcin.zukowski@gmail.com>
2021-08-23 23:49:34 -0700

    Add radial gradient generator

---
+++ purp-map-img/rad-gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from math import sqrt
+
+SIZE=256
+HALF=SIZE/2
+
+OFF=SIZE/8 * 0
+MAX=HALF
+
+with open("rad.gray", "wb") as f:
+    for y in range(0, SIZE):
+        for x in range(0, SIZE):
+            v1 = sqrt((x - HALF - OFF) ** 2 + (y - HALF) **2)
+            v1 = ((HALF-min(v1,HALF))/HALF)
+            v2 = sqrt((x - HALF + OFF) ** 2 + (y - HALF) **2)
+            v2 = ((HALF-min(v2,HALF))/HALF)
+            v1 = 255 * (v1 ** 2)
+            v2 = 255 * (v2 ** 2)
+            v = min(v1 + 0 * v2, 255)
+#            v = 255 - v;
+            if y == HALF:
+                print(y, x, v)
+            f.write(int(v).to_bytes(1, 'little'))
